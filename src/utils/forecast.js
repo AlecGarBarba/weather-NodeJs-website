15a7e6b0822ca877a8f73319c2f37a3f94ea8107
unknown <alecbarba@gmail.com>
2021-02-01 20:36:49 -0600

    precipitation added to weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,12 +1,10 @@
 const request = require('postman-request')
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
+// Goal: Add new data to forecast
+
+//update the forcaste to include new data
+//commit changes
+//push to github and deploy to Heroku
+//test
 
 
 const forecast = (latitude, longitude, callback)=>{
@@ -14,7 +12,7 @@ const forecast = (latitude, longitude, callback)=>{
     request({url, json: true, },(error, {body})=>{
         error? (callback('Unable to connect to weather service', undefined))
         : (body.error)? (callback(body.error.type + ". "+ body.error.info, undefined)) 
-        : callback(undefined, body.current.weather_descriptions +". It is currently "+body.current.temperature+" degrees celcius out. It feels like "+body.current.feelslike+" degrees out")
+        : callback(undefined, body.current.weather_descriptions +". It is currently "+body.current.temperature+" degrees celcius out. It feels like "+body.current.feelslike+" degrees out. "+ "There is currently a precipitation of: "+ body.current.precip+"%")
     })
 
     
